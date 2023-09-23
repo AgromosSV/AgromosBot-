41de29527b8f029996ddac27816d4f7b89541b23
Agromos <138736712+AgromosSV@users.noreply.github.com>
2023-09-23 10:37:35 -0400

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -5,12 +5,12 @@ if (!m.isGroup) return !1
 if (m.sender.startsWith('212' || '212')) {
 if (!m.isGroup) return !1
 db.data.users[m.sender].banned = true
-m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`✳️ Anti árabes está activo para evitar spam\n\nAdios`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}  
 if (m.sender.startsWith('265' || '265')) {
 if (!m.isGroup) return !1
 db.data.users[m.sender].banned = true
-m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`✳️ Anti árabes está activo para evitar spam\n\nAdios`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
 }
 export default handler
